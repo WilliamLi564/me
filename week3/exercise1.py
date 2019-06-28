e77264fe30a9350ed7759a44c2d9ba7baafff6f8
William Li <564999440will@gmail.com>
2019-06-28 10:53:32 +1000

    early work on this week

--- week3/exercise1.py
+++ week3/exercise1.py
@@ -12,7 +12,10 @@ def loop_ranger(start, stop=None, step=1):
     The look up the docs for range(), you can answer this with just the range 
     function, but we'd like you to do it the long way, probably using a loop.
     """
-    return None
+    my_list = []
+    for a_number in range(start, stop, step):
+        my_list.append(a_number)
+    return my_list
 
 
 def lone_ranger(start, stop, step):
@@ -20,7 +23,10 @@ def lone_ranger(start, stop, step):
 
     Look up the docs for range() and wrap it in a 1:1 way
     """
-    return None
+    my_list = []
+    for a_number in range(start, stop, step):
+        my_list.append(a_number)
+    return my_list
 
 
 def two_step_ranger(start, stop):
@@ -29,7 +35,10 @@ def two_step_ranger(start, stop):
     Sometimes you want to hide complexity.
     Make a range function that always has a step size of 2
     """
-    return None
+    my_list = []
+    for a_number in range(start, stop, 2):
+        my_list.append(a_number)
+    return my_list
 
 
 def stubborn_asker(low, high):
@@ -40,7 +49,14 @@ def stubborn_asker(low, high):
 
     Look up the docs for input
     """
-    return None
+    my_list = []
+    the_number = input("A number between low and high")
+    if the_number > low:
+        elif the_number < high:
+        my_list.append(a_number)
+    else print("Please enter a number in range")
+    return my_list
+
 
 
 def not_number_rejector(message):
--- week3/exercise3.py
+++ week3/exercise3.py
@@ -26,6 +26,29 @@ def advancedGuessingGame():
     purpose if you can!
     """
 
+    print("\nWelcome to the guessing game!")
+    print("A number between 100 and _ ?")
+    lowerBound = input("Enter a lower bound: ")
+    print("OK then, a number between 0 and {} ?".format(lowerBound))
+    lowerBound = int(lowerBound)
+
+    actualNumber = random.randint(100, lowerBound)
+
+    guessed = False
+    
+    while not guessed:
+      guessedNumber = int(input("Guess a number: "))
+      print("You guessed {},".format(guessedNumber),)
+      if guessedNumber == actualNumber:
+        print ("You got it!! It was {}".format(actualNumber))
+        guessed = True
+      elif guessedNumber is not int:
+        print("Please enter an integer!")
+      elif guessedNumber < actualNumber:
+        print("Too small, try again :'(")
+      else:
+        print("Too big, try again :'(")
+
     return "You got it!"
     # the tests are looking for the exact string "You got it!". Don't modify that!
 
